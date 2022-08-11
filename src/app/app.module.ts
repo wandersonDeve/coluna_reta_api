e99a34bcb35a1fdeec932bdb930662f86ad4b349
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-08-11 20:59:04 -0300

    feat: create table address and institution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { ScheduleModule } from '@nestjs/schedule';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import 'reflect-metadata';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRootAsync({
-      useFactory: () => ({
-        type: 'mysql',
-        host: process.env.TYPEORM_HOST || 'localhost',
-        port: Number(process.env.TYPEORM_PORT) || 3306,
-        username: process.env.TYPEORM_USERNAME,
-        password: process.env.TYPEORM_PASSWORD,
-        database: process.env.TYPEORM_DATABASE,
-        entities: [__dirname + '/../**/*.entity.{js,ts}'],
-        synchronize: false,
-        logging: true,
-      }),
-    }),
-    ScheduleModule.forRoot()
-  ],
-  controllers: [],
-  providers: [],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
+import { AppModule } from './app/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
