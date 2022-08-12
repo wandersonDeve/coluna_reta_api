120a513f116c10098632c40cbe7366762a846f8f
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-08-11 22:12:04 -0300

    feat: create route to create new address

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AddressController } from './address.controller';
+import { CreateAddressService } from './services';
 
 @Module({
   controllers: [AddressController],
+  providers: [CreateAddressService],
 })
 export class AddressModule {}
