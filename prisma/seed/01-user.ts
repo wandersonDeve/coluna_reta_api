1d6cf9255678e273bb27d6dffba0d12893e1bb31
Gustavo Martins dos Santos <gugumartin77@gmail.com>
2022-08-13 15:59:21 -0300

    feat: implement seed database (user)

---
+++ prisma/seed/01-user.ts
@@ -0,0 +1,23 @@
+import { Prisma, PrismaClient } from '@prisma/client';
+import { UserRole } from 'src/user/util/roleUser';
+
+export const users: Prisma.UserCreateInput[] = [
+  {
+    name: 'Charles Xavier',
+    role: 'ADMIN',
+    email: 'reta@1.com',
+    passwordHash: '$2a$10$6EgPRZqqBJIzIya.NknvZeJA9MN20EjSvfWgZwkf.Z6KYTM9eJm9W',
+  },
+];
+
+export const user = async (prisma: PrismaClient) => {
+  for (const obj of Object.values(users)) {
+    await prisma.user.upsert({
+      where: { email: obj.email },
+      update: {},
+      create: {
+        ...obj,
+      },
+    });
+  }
+};
