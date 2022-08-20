8c3f6e26a4862e6f1e2f8390dab09d69c08c91fd
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-08-20 18:40:58 -0300

    feat: update read me

--- prisma/seed/admin.ts
+++ prisma/seed/admin.ts
@@ -5,14 +5,16 @@ export const users: Prisma.UserCreateInput[] = [
     name: 'Charles Xavier',
     role: 'ADMIN',
     email: 'reta@1.com',
-    passwordHash: '$2a$10$6EgPRZqqBJIzIya.NknvZeJA9MN20EjSvfWgZwkf.Z6KYTM9eJm9W',
+    passwordHash:
+      '$2a$10$6EgPRZqqBJIzIya.NknvZeJA9MN20EjSvfWgZwkf.Z6KYTM9eJm9W',
   },
   {
     name: 'Scott Summers',
     role: 'BACKOFFICE',
     email: 'reta@2.com',
-    passwordHash: '$2a$10$6EgPRZqqBJIzIya.NknvZeJA9MN20EjSvfWgZwkf.Z6KYTM9eJm9W',
-  }
+    passwordHash:
+      '$2a$10$6EgPRZqqBJIzIya.NknvZeJA9MN20EjSvfWgZwkf.Z6KYTM9eJm9W',
+  },
 ];
 
 export const user = async (prisma: PrismaClient) => {
