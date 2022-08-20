5e9ab92a4531e3349ed441c21f3b8a07ddc14e06
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-08-20 14:36:41 -0300

    fix: remove duplicated files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'prisma/service/prisma.service';
-import { AddressModule } from './modules/address/address.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './modules/user/user.module';
+import { AddressModule } from './modules/address/address.module';
 import { AuthModule } from './modules/auth/auth.module';
-import { StudentModule } from './modules/student/student.module';
 import { InstitutionModule } from './modules/institution/institution.module';
+import { StudentModule } from './modules/student/student.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
