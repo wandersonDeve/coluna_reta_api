ce4d24b421f5572fd1d90825da94c8b8bc07167b
Wanderson Santos <81826043+wandersonDeve@users.noreply.github.com>
2022-08-20 14:48:06 -0300

    refactor: remove old service

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -10,13 +10,11 @@ import {
   UpdateUserService,
 } from './services';
 import { UserController } from './user.controller';
-import { UserService } from './user.service';
 
 @Module({
   imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [UserController],
   providers: [
-    UserService,
     PrismaService,
     FindAllUsersServices,
     CreateUserService,
