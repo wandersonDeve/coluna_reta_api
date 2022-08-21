2fd65958f33b8fa3bb80a6ba4c3cbf2558765dee
Gustavo Martins dos Santos <gugumartin77@gmail.com>
2022-08-21 14:16:56 -0300

    feat: implement pagination in searchUsers

--- src/modules/user/services/search-users.service.ts
+++ src/modules/user/services/search-users.service.ts
@@ -1,10 +1,26 @@
+import {
+  PageDto,
+  PageMetaDto,
+  PageOptionsDto,
+} from 'src/shared/pagination-dtos';
 import { SearchUserDto } from '../dto/search.dto';
 import { UserRepository } from '../repository/user.repository';
 
 export class SearchUsersService {
-  async execute(searchUserDto: SearchUserDto) {
+  async execute(pageOptionsDto: PageOptionsDto, searchUserDto: SearchUserDto) {
     const userRepository = new UserRepository();
 
-    return await userRepository.searchUsers(searchUserDto);
+    const users = await userRepository.searchUsers(
+      pageOptionsDto,
+      searchUserDto,
+    );
+
+    const allUsers = await userRepository.getAllUsers();
+
+    const itemCount = allUsers.length;
+
+    const pageMetaDto = new PageMetaDto({ itemCount, pageOptionsDto });
+
+    return new PageDto(users, pageMetaDto);
   }
 }
