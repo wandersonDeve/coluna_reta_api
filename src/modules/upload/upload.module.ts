43921c74308d644417bd2332f83652a91c658287
Gustavo Martins dos Santos <gugumartin77@gmail.com>
2022-09-01 23:27:36 -0300

    fix: implement jet strategy in upload module

--- src/modules/upload/upload.module.ts
+++ src/modules/upload/upload.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { FileUploadService } from './services/upload-image.service';
 import { UploadController } from './upload.controller';
 
 @Module({
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
   controllers: [UploadController],
   providers: [FileUploadService],
 })
